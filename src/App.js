55e323dbe23222b7908b7e6c6c0267dcbe45e29a
Murdho <murdho@murdho.com>
2020-01-24 08:59:45 +0200

    Add reactstrap, fix formatting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { Button } from "reactstrap";
 
 const App = () => {
   return (
-    <h1>Hello!</h1>
-  )
-}
+    <React.Fragment>
+      <h1>Hello!</h1>
 
-ReactDOM.render(<App />, document.getElementById("root"))
+      <Button color="success">Yay!</Button>
+    </React.Fragment>
+  );
+};
+
+export default App;
